5010c966b2f1f91f07180ab687bfab2e8080c16a
Nguyen Ngoc <nguyenvietngoc199717@gmail.com>
2022-09-05 10:14:45 +0700

    add file

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './import';
+import './brand.css';
+
+const Brand = () => {
+	return (
+		<div className='gpt3__brand section__padding'>
+			<img src={google} alt='google' />
+			<img src={slack} alt='slack' />
+			<img src={atlassian} alt='atlassian' />
+			<img src={dropbox} alt='dropbox' />
+			<img src={shopify} alt='shopify' />
+		</div>
+	);
+};
+
+export default Brand;
